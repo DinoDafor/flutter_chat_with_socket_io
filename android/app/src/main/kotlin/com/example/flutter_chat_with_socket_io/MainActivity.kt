250db9c4f9c4912bff18d8fa61aaa61ee2122902
DinoDafor <dinodafor@mail.ru>
2023-10-18 23:34:41 +0300

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/flutter_chat_with_socket_io/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.flutter_chat_with_socket_io
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_chat_with_socket_io/screens/login.dart';
+import 'package:provider/provider.dart';
+
+import 'providers/login.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Socket.IO',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: ChangeNotifierProvider(
+        create: (context) => LoginProvider(),
+        child: const LoginScreen(),
+      ),
+    );
+  }
+}
